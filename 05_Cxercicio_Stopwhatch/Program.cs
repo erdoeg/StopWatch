87a5f4c6b875a36d3b08efa5957862b4059e0211
Viviane Erdoeg <Viviane Erdoeg>
2017-08-01 19:08:31 -0300

    Add project files.

---
+++ 05_Cxercicio_Stopwhatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Cxercicio_Stopwhatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("--- S T O P W A T C H ---");
+                Console.WriteLine("Press ENTER to start - other key to exit");
+
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    var firstStopW = new StopWatch();
+                    firstStopW.StartWatch();
+                    Console.WriteLine("Press ENTER to stop - other key to exit");
+                    if (Console.ReadKey().Key == ConsoleKey.Enter)
+                    {
+                        firstStopW.EndWatch();
+                        firstStopW.Duration();
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\nThanks for using STOPWATCH");
+
+            
+
+
+
+
+        }
+    }
+}
---
+++ 05_Cxercicio_Stopwhatch/StopWatch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Cxercicio_Stopwhatch
+{
+    public class StopWatch
+    {
+        private readonly TimeSpan _duration;
+        private DateTime _startNow;
+        private DateTime _endNow;
+        private bool _isRunning;
+
+        public StopWatch()
+        {
+            _duration = new TimeSpan(0, 0, 0);
+        }
+
+        public void StartWatch()
+        {
+
+            if (!_isRunning)
+            {
+                _startNow = DateTime.Now;
+                _isRunning = true;
+                Console.WriteLine("STOPWATCH starts!");
+            }
+            else
+            {
+                throw new InvalidOperationException("StopWatch Already Started");
+            }
+
+        }
+
+        public void EndWatch()
+        {
+            if (_isRunning)
+            {
+                _endNow = DateTime.Now;
+                _isRunning = false;
+                Console.WriteLine("STOPWATCH ends");
+            }
+
+        }
+
+        public void Duration()
+        {
+            Console.WriteLine("Duration: " + (_endNow - _startNow).ToString());
+        }
+    }
+}
